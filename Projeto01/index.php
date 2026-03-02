2a696e3cfcd98ef49e9943810bee7b410ea80da6
Cris <cristian.marcheti@etec.sp.gov.br>
2026-03-02 09:53:34 -0300

    1 dúvida detectada

--- Projeto01/index.php
+++ Projeto01/index.php
@@ -1,41 +1,61 @@
-<?php
-setlocale(LC_TIME, "pt_BR", "pt_BR.utf-8", "pt_BR.utf-8", 'portuguese');
-date_default_timezone_set('America/Sao_Paulo');
-?>
-<!DOCTYPE html>
-<html lang="pt-br">
+<html>
+    <?php
+    setlocale(LC_TIME, "pt_BR", "pt_BR.utf-8", "pt_BR.utf-8", 'portuguese');
+    date_default_timezone_set('America/Sao_Paulo');
+    ?>
+    <!DOCTYPE html>
+    <html lang="pt-br">
 
-<head>
-    <?php include_once 'cabecalho.php'; ?>
-</head>
+    <head>
+        <?php include_once 'cabecalho.php'; ?>
+    </head>
+    
+    <body>
+        
+        <?php include_once 'navbar.php'; ?>
 
-<body>
-    <?php include_once 'navbar.php'; ?>
-
-    <div class="container mt-3">
-        <div class="alert alert-primary" role="alert">
+        <div class="container mt-3">
             <?php
-            echo "Seja bem vindo";
+            
+            // Captura e sanitiza o parâmetro
+            $pagina = filter_input(INPUT_GET, 'p', FILTER_SANITIZE_SPECIAL_CHARS);
+            
+            // Define páginas permitidas (lista branca)
+            
+            $paginasPermitidas = [
+                'index' => 'home.php',
+                'home' => 'home.php',
+                'sobre' => 'sobre.php',
+                'contato' => 'contato.php',
+                'produtos' => 'produtos.php'
+            ];
+            
+            // Página padrão
+            if (empty($pagina)) {
+                $pagina = 'index';
+            }
+            
+            // Verifica se está na lista branca
+            if (array_key_exists($pagina, $paginasPermitidas)) {
+                include_once $paginasPermitidas[$pagina];
+                
+                } else {
+                    http_response_code(404);
+                ?>
+                
+                <div class="alert alert-danger" role="alert">
+                    Erro 404 - Página não encontrada!
+                </div>
+                <?php
+
+            }
+
             ?>
+            
         </div>
+        
+        <?php include_once "scripts.php"; ?>
 
-        <?php
-        $var = 10;
-        if ($var == 10) {
-            $mgs = 'Igual a 10';
-        }else if ($var > 10) {
-            $mgs = 'maior que 10';
-        }else{
-            $mgs = 'menor que 10';
-        }
-
-        $mgs.= ' teste';
-
-        echo $mgs;
-        ?>
-    </div>
-
-    <?php include_once "scripts.php"; ?>
-</body>
+    </body>
 
 </html>
--- Projeto01/navbar.php
+++ Projeto01/navbar.php
@@ -7,16 +7,11 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="?p=home">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="?p=categoria/list">Categorias</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
         </li>
       </ul>
     </div>
