711c61f4d11f64bfba383a1099e72598cab2d619
Cris <marcheticristian@gmail.com>
2026-07-27 14:39:46 -0300

    Exemplo crud cat sem procedures

--- Projeto01/models/Categoria.php
+++ Projeto01/models/Categoria.php
@@ -69,7 +69,8 @@ class Categoria {
             echo $erro->getMessage();
         }
     }
-
+    
+    // métodos sem procedure
     public function excluir()
     {
         try{
@@ -82,8 +83,120 @@ class Categoria {
             echo $erro->getMessage();
         }
     }
-<<<<<<< HEAD
+    
+    public function inserir()
+    {
+        try {
+            $this->conn = new Conn();
+            $sql = "INSERT INTO categoria VALUES (?, ?, ?)";
+            $executar = $this->conn->prepare($sql);
+            $executar->bindValue(1, $this->id);
+            $executar->bindValue(2, mb_strtoupper($this->nome));
+            $executar->bindValue(3, mb_strtoupper($this->informacoes));
+            return $executar->execute() == 1 ? true : false;
+        } catch (PDOException $erro){
+            echo $erro->getMessage();
+        }
+    }
 
-=======
->>>>>>> 1cc02b6b8f24ccb38eb4993fef0b1dfceb3b3348
+    public function alterar()
+    {
+        try {
+            $this->conn = new Conn();
+            $sql = "UPDATE categoria 
+                    SET nome = ?, informacoes = ?,
+                    WHERE id = ?";
+            $executar = $this->conn->prepare($sql);
+            $executar->bindValue(1, mb_strtoupper($this->nome));
+            $executar->bindValue(2, mb_strtoupper($this->informacoes));
+            $executar->bindValue(3, $this->id); 
+            return $executar->execute() == 1 ? true :false;
+        } catch (PDOException $erro){
+            echo $erro->getMessage();
+        }
+    }
+
+    public function listarSemProcedure()
+    {
+        try {
+            $this->conn = new Conn();
+            $sql = "SELECT * FROM {$this->tabela} ORDER BY nome";
+            $executar = $this->conn->prepare($sql); 
+            return $executar->execute() == 1 ? true :false;
+        } catch (PDOException $erro){
+            echo $erro->getMessage();
+        }
+    }
+
+    public function consultarPorID()
+    {
+        try {
+            $this->conn = new Conn();
+            $sql = "SELECT * FROM {$this->tabela} WHERE id = ?";
+            $executar = $this->conn->prepare($sql);
+            $executar->bindValue(1, $this->id); 
+            return $executar->execute() == 1 ? true :false;
+        } catch (PDOException $erro){
+            echo $erro->getMessage();
+        }
+    }
+
+    public function crudPhp($opcao)
+    {
+        try {
+
+            $this->con = new Conn();
+
+            switch ($opcao) {
+
+                case 'I':
+
+                    $sql = "INSERT INTO {$this->tabela}
+                        (nome, informacoes)
+                        VALUES (?, ?)";
+
+                    $executar = $this->con->prepare($sql);
+
+                    $executar->bindValue(1, mb_strtoupper($this->nome));
+                    $executar->bindValue(2, mb_strtoupper($this->uf));
+
+                    break;
+
+                case 'A':
+
+                    $sql = "UPDATE {$this->table}
+                           SET nome = ?,
+                               informacoes = ?
+                         WHERE id = ?";
+
+                    $executar = $this->con->prepare($sql);
+
+                    $executar->bindValue(1, mb_strtoupper($this->nome));
+                    $executar->bindValue(2, mb_strtoupper($this->uf));
+                    $executar->bindValue(3, $this->id);
+
+                    break;
+
+                case 'E':
+
+                    $sql = "DELETE FROM {$this->tabela}
+                        WHERE id = ?";
+
+                    $executar = $this->con->prepare($sql);
+
+                    $executar->bindValue(1, $this->id);
+
+                    break;
+
+                default:
+                    return false;
+            }
+
+            return $executar->execute() == 1 ? true : false;
+        } catch (PDOException $exc) {
+
+            echo $exc->getMessage();
+        }
+    }
+
 }
--- Projeto01/views/categoria/excluir.php
+++ Projeto01/views/categoria/excluir.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
     $id = filter_input(INPUT_GET, 'id');
 
@@ -9,18 +8,6 @@
 
         if ($cat->excluir()) {
     ?>
-=======
-    <?php 
-    $id = filter_input(INPUT_GET, 'id');
-
-    if($id){
-        include_once '../models/Categoria.php';
-        $cat = new Categoria();
-        $cat -> setId($id);
-
-        if($cat->excluir()){
-     ?>
->>>>>>> 1cc02b6b8f24ccb38eb4993fef0b1dfceb3b3348
             <div class="alert alert-primary" role="alert">
                 Excluído com sucesso
             </div>
@@ -28,8 +15,4 @@
         }
     }
     ?>
-<<<<<<< HEAD
     <meta http-equiv="refresh" CONTENT="1;URL=?p=categorias">
-=======
-    <meta http-equiv="refresh" CONTENT="1.5;URL=?p=categorias"
->>>>>>> 1cc02b6b8f24ccb38eb4993fef0b1dfceb3b3348
